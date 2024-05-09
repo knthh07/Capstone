9116e35e33e1c795efa5dec8d057f5ebfdc0efc6
unknown <christianjamesaddun@gmail.com>
2024-05-09 20:14:16 +0800

    Update Login no API Integration

--- src/Components/login/Login.jsx
+++ src/Components/login/Login.jsx
@@ -1,46 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import showPassword from "../pages/profile/showPassword";
-import logo from '../../assets/img/login-logo2.png';
-import './login.css';
+import logo from "../../assets/img/login-logo2.png";
+import "./login.css";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div className="loginPage">
-            <div className="leftSide">
-                <div className="nuLogo">
-                    <img src={logo} alt="NU LOGOHP" id="nuLogo"/> 
-                </div>
-            </div>
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
 
-            <div className="rightSide">
-                <div className="loginForm">
-                    <form id="loginForm" method="post">
-                        <div id="input">
-                            <h2> LOGIN </h2>
+  const loginAuth = () => {
+    const { email, password } = formData;
+    if (!email || !password) {
+      return alert("Please enter email and password!");
+    }
+    // Gawa kau ng REST API sa backend(BE) tas ipasa nyo ung state na formData sa BE nyu gagawin validations nyu d2 kau bubutasin ng mga panel
+    return navigate("/dashboard");
+  };
 
-                            <div className="loginFields">
-                                <div className="usernameField">
-                                    <label htmlFor="username">Email</label>
-                                    <input type="text" name="email" placeholder="Email" required />
-                                </div>
-                                <div className="passwordField">
-                                    <label htmlFor="password">Password</label>
-                                    <input type="password" name="password" placeholder="Password" id="pwd" required />
-                                    <input type="checkbox" onClick={showPassword} id="showPasswordbox" />
-                                </div>
-                            </div>
-
-                            <a href="forgot_Password.php" id="forgot">Forgot Password?</a>
-                            <div className="buttonsDiv">
-                                <input type="submit" value="Login" className="buttonLogin" />
-                            </div>
-                            <p>Don't have an account? <a href="signup" id="signup"> Sign up Now</a></p>
-                        </div>
-                    </form>
-                </div>
-            </div>
+  return (
+    <div className="loginPage">
+      <div className="leftSide">
+        <div className="nuLogo">
+          <img src={logo} alt="NU LOGOHP" id="nuLogo" />
         </div>
-    );
-}
+      </div>
+
+      <div className="rightSide">
+        <div className="loginForm">
+          <div id="input">
+            <h2> LOGIN </h2>
+
+            <div className="loginFields">
+              <div className="usernameField">
+                <label htmlFor="username">Email</label>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  onChange={(e) => {
+                    setFormData({ ...formData, email: e.target.value });
+                  }}
+                />
+              </div>
+              <div className="passwordField">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  id="pwd"
+                  onChange={(e) => {
+                    setFormData({ ...formData, password: e.target.value });
+                  }}
+                />
+                <input
+                  type="checkbox"
+                  onClick={showPassword}
+                  id="showPasswordbox"
+                />
+              </div>
+            </div>
+
+            <a href="forgot_Password.php" id="forgot">
+              Forgot Password?
+            </a>
+            <div className="buttonsDiv">
+              <input
+                type="submit"
+                value="Login"
+                className="buttonLogin"
+                onClick={() => {
+                  loginAuth();
+                }}
+              />
+            </div>
+            <p>
+              Don't have an account?{" "}
+              <a href="signup" id="signup">
+                {" "}
+                Sign up Now
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
